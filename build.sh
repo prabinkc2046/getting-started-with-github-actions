e4a83ffd4ce13ea62e44334ff9391b8726563bad
Prabin K C <106384763+prabinkc2046@users.noreply.github.com>
2024-02-08 23:19:25 +1100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,24 +1,19 @@
-# Print the contents of the current directory
-echo "Contents of the current directory:"
-ls
-echo
+# Step 6: Additional Deployment Steps (Optional)
+- name: Additional Deployment Steps
+  run: |
+    # Change directory to 'githubactions'
+    echo "Changing directory to githubactions..."
+    cd githubactions
+    echo 
 
-# Change directory to 'githubactions'
-echo "Changing directory to githubactions..."
-cd githubactions
-echo 
+    # Install dependencies using npm
+    echo "Installing dependencies..."
+    npm install 
+    echo
 
-# Install dependencies using npm
-echo "Installing dependencies..."
-npm install 
-echo
+    # Build the React app
+    echo "Building React app..."
+    npm run build
+    echo
 
-# Build the React app
-echo "Building React app..."
-npm run build
-echo
-
-# Content of current dir
-echo "Content of current dir"
-ls
-echo 
+   
