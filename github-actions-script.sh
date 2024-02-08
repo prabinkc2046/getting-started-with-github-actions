2d3250eb59a4ace7ab2e903ebc65b876a3ff1b1c
Prabin K C <106384763+prabinkc2046@users.noreply.github.com>
2024-02-08 20:37:49 +1100

    Update github-actions-script.sh

--- github-actions-script.sh
+++ github-actions-script.sh
@@ -1,9 +1,4 @@
 echo "CD into githubactions"
 echo
-echo "Installing dependencies"
-npm install
-echo "Building ..."
-npm run build
-echo
-echo "Content of the build file"
-ls build
+echo "content of current work directory"
+ls 
