96727375cd7775a659167c0fe36fd731952ecd09
Prabin K C <106384763+prabinkc2046@users.noreply.github.com>
2024-02-08 20:52:05 +1100

    Update github-actions-script.sh

--- github-actions-script.sh
+++ github-actions-script.sh
@@ -8,13 +8,13 @@ echo Content of build folder
 ls build
 echo
 echo "check if sshpass is present"
-if command -v sshpass; then
-  sshpass -p prabin scp -r build/* prabin@172.105.178.89:/var/www/html/test
+if command -v sshpass >> /dev/null; then
+  sshpass -p prabin scp -o StrictHostKeyChecking=no -r build/* prabin@172.105.178.89:/var/www/html/test
   echo "build is transferred to production server"
 else
   echo "sshpass is not found, installing"
   apt update -y && apt install sshpass -y
-  sshpass -p prabin scp -r build/* prabin@172.105.178.89:/var/www/html/test
+  sshpass -p prabin scp -o StrictHostKeyChecking=no -r build/* prabin@172.105.178.89:/var/www/html/test
   echo "build is transferred to production server"  
 fi
 
