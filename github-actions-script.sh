5700513f048a8b6bbc14a8acc788ddc845bc887e
Prabin K C <106384763+prabinkc2046@users.noreply.github.com>
2024-02-08 20:49:07 +1100

    Update github-actions-script.sh

--- github-actions-script.sh
+++ github-actions-script.sh
@@ -6,3 +6,15 @@ echo building production grade react app
 npm run build
 echo Content of build folder
 ls build
+echo
+echo "check if sshpass is present"
+if command -v sshpass; then
+  sshpass -p prabin scp -r build/* prabin@172.105.178.89:/var/www/html/test
+  echo "build is transferred to production server"
+else
+  echo "sshpass is not found, installing"
+  apt update -y && apt install sshpass -y
+  sshpass -p prabin scp -r build/* prabin@172.105.178.89:/var/www/html/test
+  echo "build is transferred to production server"  
+fi
+
